d1a6040a5c7a119de964d6e5ef182f70160fd1dc
Pranay2699 <37505757+Pranay2699@users.noreply.github.com>
2020-04-09 14:30:06 +0530

    Changes done

--- farmup/shopping_cart/urls.py
+++ farmup/shopping_cart/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.urls import path,include
 from . import views
 
-app_name = "crop_selling_portal"
+app_name = "shopping_cart"
 
 urlpatterns = [
     path('buyingportal/', views.buyingpage, name='buyingpage'),
