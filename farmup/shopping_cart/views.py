bb4378b15884f96461ab5438cff343d80a6ead94
saisantoshchirag <saisantosh.c17@iiits.in>
2020-04-09 13:28:28 +0530

    changed shopping_cart.views
    added some crops

--- farmup/shopping_cart/views.py
+++ farmup/shopping_cart/views.py
@@ -3,4 +3,21 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
 def buyingpage(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Products')
+    names = ['tomato','beans','cabbage','rice','wheat','almonds']
+    rates = [25,60,30,50,75,1000]
+    available = [7,5,25,10,6,5]
+    # for i in range(6,12):
+    #     response = table.put_item(
+    #         Item={
+    #             'product_id':i,
+    #             'product_name':names[i-6],
+    #             'product_cost':rates[i-6],
+    #             'available_amount':available[i-6]
+    #         }
+    #     )
+    response = table.scan()
+    data = response['Items']
+    print(data)
     return render(request,'shopping_cart/shop.html')
